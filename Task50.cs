c19315ae31c61e76f003b3b7fb147e82ea131d32
yurrikon <yurrikon@gmail.com>
2022-07-03 01:43:05 +0300

    task50 a

--- Task50.cs
+++ Task50.cs
@@ -4,3 +4,8 @@
 
 Console.WriteLine();
 
+int[,] array 
+Console.WriteLine("Введите номер строки:");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца:");
+int column = Convert.ToInt32(Console.ReadLine());
