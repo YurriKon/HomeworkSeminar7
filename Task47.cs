6d201a611422cbe12f0fd8c5816e93cdc8d838db
yurrikon <yurrikon@gmail.com>
2022-07-03 01:26:40 +0300

    task47

---
+++ Task47.cs
@@ -0,0 +1,21 @@
+// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами, 
+// округлёнными до одного знака.
+
+double[,] array = new double[4,4];
+Random rand = new Random();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = Math.Round((rand.NextDouble()*100 - 50), 1);
+    }
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i,j] + "\t");
+    }
+}
+
